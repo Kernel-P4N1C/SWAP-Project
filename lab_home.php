2cbdb6b61b46c30dffccdfabeddb767233e1554e
Ebbyz <2304292B@student.tp.edu.sg>
2024-11-17 20:15:16 +0800

    Add files via upload

---
+++ lab_home.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="lab_home.css">
+</head>
+<body>
+
+    <header>
+        <nav>
+            <div class="logo">
+                <a href="#">Labenue</a>
+            </div>
+            <ul class="nav_link">
+                <li><a href="#">Student Profiles</a></li>
+                <li><a href="#">Lab Equipment</a></li>
+                <li><a href="#">Inventory</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section class="sec">
+        <div class="sec_t">
+            <h1>Managing Equipment, Made Easy</h1>
+            <p>Science can be Fun Too</p>
+            <a href="#others" class="btn">Explore Other Pages</a>
+        </div>
+    </section>
+
+    <section id="ch_page" class="ch_page">
+        <h2>Services</h2>
+        <div class="ch_page_card">
+            <div class="card">
+                <h3>Student Profiles</h3>
+                <p>Manage Student Profiles</p>
+            </div>
+            <div class="card">
+                <h3>Lab Equipment</h3>
+                <p>Manage Lab Equipment</p>
+            </div>
+            <div class="card">
+                <h3>Invetory</h3>
+                <p>Manage Inventory</p>
+            </div>
+        </div>
+    </section>
+
+    <section id="others" class="others">
+        <h2>Other Pages</h2>
+        <ul>
+            <li><a href="#">Student Profiles</a></li>
+            <li><a href="#">Lab Equipment</a></li>
+            <li><a href="#">Inventory</a></li>
+        </ul>
+    </section>
+    <footer>
+            <p>Labenue. Your Favourite Lab Management Service</p>
+            <ul class="social_links">
+                <li><a href="#">Instagram</a></li>
+                <li><a href="#">X</a></li>
+                <li><a href="#">LinkedIn</a></li>
+            </ul>
+    </footer>
+
+</body>
+</html>
